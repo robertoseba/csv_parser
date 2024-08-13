0180ae1f33022709dca35f975677d9d97266aa8c
Roberto Seba <roberto@robertoseba.com>
2024-08-13 11:25:37 -0300

    feat: creates flags for cli and helper for genereting dummie data

--- main.go
+++ main.go
@@ -1,38 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
+	"flag"
 
 	"github.com/robertoseba/csv_parser/cmd/app"
 )
 
 func main() {
-	args := os.Args[1:]
+	var colFilterFlag = flag.String("filter", "", "Filter the CSV file by the specified columns")
+	var colRulesFlag = flag.String("rules", "", "Apply rules to the specified columns. Ex: -rules \"col1:eq(100)\"")
+	flag.Parse()
 
-	if len(args) == 0 {
-		app.Run("./data.csv", "col1,col2,col3", "col1:eq(row_1000)||eq(row_11);col2:gte(174)")
-		return
+	filename := flag.Arg(0)
+
+	// TODO: Implement reading from stdin
+	if filename != "" {
+		app.Run(filename, *colFilterFlag, *colRulesFlag)
 	}
 
-	if args[0] == "--generate-csv" {
-		fmt.Printf("Generating CSV file for %s columns and %s rows...\n", args[1], args[2])
-
-		rowCount, err := strconv.Atoi(args[2])
-		if err != nil {
-			fmt.Println("Invalid row count")
-			panic(err)
-		}
-
-		columnCount, err := strconv.Atoi(args[1])
-		if err != nil {
-			fmt.Println("Invalid column count")
-			panic(err)
-		}
-
-		generateCSV("./data.csv", columnCount, rowCount)
-
-		fmt.Println("CSV file generated successfully")
-	}
 }
--- pkg/generator/main.go
+++ pkg/generator/main.go
@@ -3,12 +3,13 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
 )
 
-func generateCSV(filename string, columnCount int, rowCount int) {
+func generateCSV(filename string, columnCount uint, rowCount uint) {
 	file, err := os.Create(filename)
 	if err != nil {
 		panic(err)
@@ -28,12 +29,12 @@ func generateCSV(filename string, columnCount int, rowCount int) {
 	}
 }
 
-func randomData(columns int, rows int) [][]string {
-	data := make([][]string, rows)
+func randomData(columns uint, rows uint) [][]string {
+	data := make([][]string, rows+1)
 
 	data[0] = make([]string, columns)
 
-	for i := 0; i < columns; i++ {
+	for i := 0; i < int(columns); i++ {
 		data[0][i] = "col" + fmt.Sprintf("%d", i+1)
 	}
 
@@ -44,7 +45,7 @@ func randomData(columns int, rows int) [][]string {
 
 		data[i] = make([]string, columns)
 
-		for j := 0; j < columns; j++ {
+		for j := 0; j < int(columns); j++ {
 			if j == 0 {
 				data[i][j] = "row_" + fmt.Sprintf("%d", i)
 				continue
@@ -54,4 +55,19 @@ func randomData(columns int, rows int) [][]string {
 		}
 	}
 	return data
+}
+
+func main() {
+	var cols = flag.Uint("cols", 2, "Number of columns to be generated")
+	var rows = flag.Uint("rows", 10, "Number of rows to be generated")
+	flag.Parse()
+
+	filename := flag.Arg(0)
+
+	if filename == "" {
+		generateCSV("./test.csv", *cols, *rows)
+		os.Exit(0)
+	}
+
+	generateCSV(filename, *cols, *rows)
 }
