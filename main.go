fb39c2e75b84656d0a2957048d299600bc8e0582
Roberto Seba <roberto@robertoseba.com>
2024-08-18 16:30:51 -0300

    perf: creating profiler for optimization

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"flag"
+	"os"
+	"runtime/pprof"
 
 	"github.com/robertoseba/csv_parser/cmd/app"
 )
 
 func main() {
+	f, _ := os.Create("cpu.pprof")
+	defer f.Close()
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
 	var colFilterFlag = flag.String("filter", "", "Filter the CSV file by the specified columns")
 	var colRulesFlag = flag.String("rules", "", "Apply rules to the specified columns. Ex: -rules \"col1:eq(100)\"")
 	var orderedFlag = flag.Bool("ordered", false, "Print the rows in the same order as they appear in the file")
