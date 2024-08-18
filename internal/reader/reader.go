955c4367d4c1607d0a07c6ada3b9b475fc789c19
Roberto Seba <roberto@robertoseba.com>
2024-08-18 13:34:03 -0300

    wip

--- internal/reader/reader.go
+++ internal/reader/reader.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"sync"
 
 	"github.com/robertoseba/csv_parser/internal/parser"
 	"github.com/robertoseba/csv_parser/internal/row"
@@ -85,29 +86,34 @@ func (r *CsvReader) read() {
 }
 
 func (r *CsvReader) processRecords() {
+	wg := sync.WaitGroup{}
 
 	for record := range r.readChan {
+		wg.Add(1)
+		go func() {
+			row := row.NewRow(r.currentLine, r.headers.Values(), record)
 
-		row := row.NewRow(r.currentLine, r.headers.Values(), record)
+			if r.config.ColRules == nil {
+				r.currentLine++
+				r.outputChan <- row.Only(r.config.ColFilters).Values()
+			} else {
+				//TODO: How do colRules interact between them? If one is valid, should we return the row?
+				// Should we define the logical operator for interaction between columns? EX: (OR)col1:eq(5)||lte(10);col2:gte(10)
+				// Currently we are assuming that all columns' rules must be valid to return the row
 
-		if r.config.ColRules == nil {
-			r.currentLine++
-			r.outputChan <- row.Only(r.config.ColFilters).Values()
-		}
-		//TODO: How do colRules interact between them? If one is valid, should we return the row?
-		// Should we define the logical operator for interaction between columns? EX: (OR)col1:eq(5)||lte(10);col2:gte(10)
-		// Currently we are assuming that all columns' rules must be valid to return the row
-
-		approved := true
-		for _, colRule := range r.config.ColRules {
-			if !colRule.IsValid(row) {
-				approved = false
+				approved := true
+				for _, colRule := range r.config.ColRules {
+					if !colRule.IsValid(row) {
+						approved = false
+					}
+				}
+				if approved {
+					r.currentLine++
+					r.outputChan <- row.Only(r.config.ColFilters).Values()
+				}
 			}
-		}
-		if approved {
-			r.currentLine++
-			r.outputChan <- row.Only(r.config.ColFilters).Values()
-		}
+			wg.Done()
+		}()
 	}
 	close(r.outputChan)
 }
